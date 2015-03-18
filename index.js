d88e1e45052467b72fcd530ebfdd7c1d11f90fa7
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-18 08:06:26 -0400

    expose `sync`, async fixes

--- index.js
+++ index.js
@@ -7,40 +7,85 @@
 
 'use strict';
 
+/**
+ * Module dependencies
+ */
+
 var path = require('path');
 var chalk = require('chalk');
-var async = require('async');
 var symbol = require('log-symbols');
 var parse = require('parse-github-url');
 var stringify = require('stringify-github-url');
+var pkg = require('load-module-pkg');
 var config = require('load-pkg');
 var mdu = require('markdown-utils');
-var get = require('get-pkgs');
 
-module.exports = function reflinks(repos, patterns, cb) {
+/**
+ * Expose `reflinks`
+ */
+
+module.exports = reflinks;
+
+/**
+ * Generate a reflink or list of reflinks for npm modules.
+ *
+ *   - If no repo names are passed, reflinks are generated for all locally-installed
+ *     dependencies listed in package.json
+ *   - If names are passed, reflinks are generated both from matching locally-
+ *     installed dependencies and, if necessary, by querying npm.
+ *
+ * @param  {Array} repos [description]
+ * @param  {Function} cb [description]
+ * @return {Array}
+ */
+
+function reflinks(repos, options, cb) {
   if (typeof repos === 'function') {
     cb = repos;
-    patterns = null;
-    repo = null;
+    options = {};
+    repos = null;
+  }
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
   }
 
-  if (typeof patterns === 'function') {
-    cb = patterns;
-    patterns = null;
-  }
+  options = options || {};
 
-  if (repos && typeof repos !== 'string' && !Array.isArray(repos)) {
+  if (typeof repos !== 'string' && !Array.isArray(repos)) {
     throw new TypeError('helper-reflinks expects a string or array.');
   }
 
-  if (!repos) {
-    return cb(null, listDeps(config));
+  var deps = reflinks.sync(options);
+
+  if (!repos || !repos.length) {
+    if (typeof cb === 'function') {
+      return cb(null, deps);
+    }
+    return deps;
   }
 
-  var msg = '  helper-reflinks: getting reference links from npm.';
-  console.log('  ' + symbol.success + chalk.gray(msg));
+  // generate reflinks from npm packages
+  getRepos(repos, options, cb);
+};
 
-  get(repos, patterns, function (err, pkgs) {
+reflinks.sync = function (options) {
+  message('node_modules', options);
+  return linkifyDeps(config);
+};
+
+function getRepos(repos, options, cb) {
+  var async = require('async');
+  var get = require('get-pkgs');
+
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+
+  message('npm', options);
+
+  get(repos, function (err, pkgs) {
     if (err) {
       console.error(chalk.red('helper-reflinks: %j'), err);
       return cb(err);
@@ -62,42 +107,53 @@ function linkify(repo, url) {
 }
 
 function listDeps(pkg) {
+  var deps = Object.keys(pkg.dependencies) || [];
+  return deps.sort();
+}
+
+function linkifyDeps(pkg) {
   if (!pkg.dependencies) {
     return null;
   }
 
-  var deps = Object.keys(pkg.dependencies).sort();
+  var deps = listDeps(pkg);
   var len = deps.length, i = 0;
   var res = '';
 
   while (len--) {
     var dep = deps[i++];
-    var pkg = npm(dep);
-    var ref = homepage(pkg);
-
-    res += linkify(ref.repo, ref.url)
+    var ele = node_modules(dep);
+    var ref = homepage(ele);
+    if (ref) {
+      res += linkify(ref.repo, ref.url);
+    }
   }
   return res;
 }
 
-listDeps(config);
-
-function npm(name) {
+function node_modules(name) {
   try {
     var fp = path.resolve('node_modules', name, 'package.json');
     return require(fp);
   } catch(err) {}
-  return null;
+  return {};
 }
 
 function homepage(pkg) {
   var res = {};
+  if (!pkg.repository) return null;
   if (typeof pkg.repository === 'string') {
     res = parse(pkg.repository);
   } else if (typeof pkg.repository === 'object') {
     res = parse(pkg.repository.url);
   }
-
-  res.url = stringify(res.user, res.repo)
+  res.url = stringify(res.user, res.repo);
   return res;
+}
+
+function message(place, options) {
+  if (options && options.silent !== true) {
+    var msg = '  helper-reflinks: generating reflinks from ' + place + ' info.';
+    console.log('  ' + symbol.success + chalk.gray(msg));
+  }
 }
