407347caad833d0a25e10030d8fc2ada86b0dc7b
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-31 01:13:58 -0400

    add helpers for docs

--- verbfile.js
+++ verbfile.js
@@ -2,6 +2,14 @@
 
 var verb = require('verb');
 
+verb.helper('shorten', function (str) {
+  return str.split('helper-').join('');
+});
+
+verb.helper('tag', function (str) {
+  return '{%= ' + str;
+});
+
 verb.task('default', function() {
   verb.src('.verb*.md')
     .pipe(verb.dest('./'));
