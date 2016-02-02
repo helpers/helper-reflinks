6b50bda451192ef022abbe0daf55a8bffc121907
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-02 04:48:13 -0500

    update deps

--- test.js
+++ test.js
@@ -33,22 +33,22 @@ describe('async', function() {
     reflinks('').should.match(/async/);
   });
 
-  it('should generate reflinks for a repo:', function (cb) {
-    reflinks('async', function  (err, res) {
-      res.should.equal('[async]: https://github.com/caolan/async');
+  it('should generate reflinks for a repo:', function(cb) {
+    reflinks('async', function(err, res) {
+      res.should.equal('[async]: https://github.com/caolan/async\n');
       cb();
     });
   });
 
-  it('should work as an async helper:', function (cb) {
-    render('<%= reflinks() %>', {imports: {reflinks: reflinks}}, function (err, res) {
+  it('should work as an async helper:', function(cb) {
+    render('<%= reflinks() %>', {imports: {reflinks: reflinks}}, function(err, res) {
       res.should.match(/async/);
       cb();
     })
   });
 
-  it('should combine both node_modules and with specified repos:', function (cb) {
-    render('<%= reflinks("", {node_modules: true}) %>', {imports: {reflinks: reflinks}}, function (err, res) {
+  it('should combine both node_modules and with specified repos:', function(cb) {
+    render('<%= reflinks("", {node_modules: true}) %>', {imports: {reflinks: reflinks}}, function(err, res) {
       res.should.match(/async/);
       res.should.match(/load-pkg/);
       cb();
@@ -56,31 +56,31 @@ describe('async', function() {
   });
 });
 
-describe('sync', function () {
-  beforeEach(function () {
+describe('sync', function() {
+  beforeEach(function() {
     reflinks = helper();
   });
 
-  it('should return a formatted reflinks statement:', function () {
+  it('should return a formatted reflinks statement:', function() {
     reflinks.sync('').should.match(/async/);
   });
 
-  it('should work as a lodash helper:', function () {
+  it('should work as a lodash helper:', function() {
     var actual = _.template('<%= reflinks("") %>', {imports: {reflinks: reflinks}})();
     actual.should.match(/async/);
   });
 
-  it('should work as a handlebars helper:', function () {
+  it('should work as a handlebars helper:', function() {
     handlebars.registerHelper('reflinks', reflinks);
     handlebars.compile('{{reflinks ""}}')().should.match(/async/);
   });
 });
 
 
-describe('helper', function () {
+describe('helper', function() {
   this.slow(500);
 
-  beforeEach(function () {
+  beforeEach(function() {
     app = assemble();
     app.engine('hbs', require('engine-handlebars'));
     app.engine('md', require('engine-base'));
@@ -93,43 +93,43 @@ describe('helper', function () {
     app.asyncHelper('reflinks', helper(app.options));
   });
 
-  it('should work with engine-handlebars:', function (cb) {
+  it('should work with engine-handlebars:', function(cb) {
     this.timeout(2000);
     app.page('abc', {content: 'foo {{reflinks list}} bar'})
-      .render({list: ['micromatch']}, function (err, res) {
+      .render({list: ['micromatch']}, function(err, res) {
         if (err) return cb(err);
         res.content.should.match(/\[micromatch\]/);
         cb();
       });
   });
 
-  it('should use global options', function (cb) {
+  it('should use global options', function(cb) {
     this.timeout(2000);
     app.option('remove', ['flflflfl']);
     app.page('abc', {content: 'foo {{reflinks list}} bar'})
-      .render({list: ['micromatch', 'flflflfl']}, function (err, res) {
+      .render({list: ['micromatch', 'flflflfl']}, function(err, res) {
         if (err) return cb(err);
         res.content.should.match(/\[micromatch\]/);
         cb();
       });
   });
 
-  it('should work with engine-base:', function (cb) {
+  it('should work with engine-base:', function(cb) {
     this.timeout(2000);
     app.post('xyz', {content: 'foo <%= reflinks(list) %> bar'})
-      .render({list: ['micromatch']}, function (err, res) {
+      .render({list: ['micromatch']}, function(err, res) {
         if (err) return cb(err);
         res.content.should.match(/\[micromatch\]/);
         cb();
       });
   });
 
-  it('should work using values from the context:', function (cb) {
+  it('should work using values from the context:', function(cb) {
     this.timeout(2000);
 
     app.data('list', ['micromatch']);
     app.post('xyz', {content: 'foo <%= reflinks(list) %> bar'})
-      .render(function (err, res) {
+      .render(function(err, res) {
         if (err) return cb(err);
         res.content.should.match(/\[micromatch\]/);
         cb();
